935c0f359f748d65ba526d9fd3e20c273c9734e3
Gerrard Xiong <chengcheng.xiong@my.jcu.edu.au>
2023-01-27 19:38:39 +1000

    Finding Habitable Planets

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanet = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+};
 
 fs.createReadStream('Kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
   })
   .on('error', (err) => {
     console.log(err);
   })
   .on('end', () => {
-    console.log(results);
-    console.log('done');
+    console.log(`${habitablePlanet.length} habitable planets found!`);
   });
-//parse();
